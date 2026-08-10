045295ceb8c5f9ca34ad614c8479ad684b911036
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-10 18:22:43 +0000

    added post request

--- week 2/expressAndBackend.js
+++ week 2/expressAndBackend.js
@@ -3,10 +3,21 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
+
+app.post("/conversation", function(req, res){
+    console.log(req.body)
+    console.log(req.headers)
+    console.log(req.headers["authorization"])
+    res.send({
+        msg: "2 + 2 = 4"
+    })
+})
 app.listen(PORT,function() {
     console.log(`Server started on port ${PORT}`);
 });
