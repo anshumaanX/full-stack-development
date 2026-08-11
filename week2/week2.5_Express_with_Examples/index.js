89200b927e20c3e3bb965c30f530063eb14dbbf7
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-11 16:48:18 +0000

    created basic counter in express

--- week2/week2.5_Express_with_Examples/index.js
+++ week2/week2.5_Express_with_Examples/index.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express()
 
-const port = 4000;
+const port = 3000;
 
+function findSum(n){
+  let sum = 0;
+  for(let i = 0; i<=n; i ++){
+    sum += i
+  }
+  return sum
+}
+console.log(findSum(100))
 
-app.listen(port, ()=>{
+app.get('/', function(req,res){
+  const n = req.query.n
+  const ans = findSum(n)
+  if(n!= undefined && parseInt(n)){  
+    res.json({
+      result: `The sum from 0 to ${n} is: ${ans}`
+    })
+  }
+  else{
+    res.send(
+      "Enter the value of n for calculating the sum in query parameter"
+    )
+  }
+})
+
+app.listen(port, function(){
   console.log(`Server is listining at port ${port}`)
 })
