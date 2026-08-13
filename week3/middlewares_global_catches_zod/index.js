81f044f9d2f5f0fb57a9054bade9d2b98263272f
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-13 16:47:28 +0000

    added auth checking middleware

--- week3/middlewares_global_catches_zod/index.js
+++ week3/middlewares_global_catches_zod/index.js
@@ -3,18 +3,58 @@ const app = express()
 
 const port = 3000;
 
-app.get('/health-checkup', (req,res) => {
-  
+app.use(express.json())
+
+const users = [{
+  pass: 11,
+  name: "Oggy",
+  kidney: [
+    {
+      isHealthy: false,
+    }, {
+      isHealthy: true
+    }
+  ]
+}, {
+  pass: 22,
+  name: "Jack",
+  kidney: [{
+    isHealthy: true
+  }]
+}]
+
+function authUser(req, res, next) {
+  if(!req.body){
+    console.log(req.body)
+    return res.status(411).json({
+      message: "Please input name and pass"
+    })
+  }
+  const { name, pass } = req.body;
+
+  for (let i = 0; i < users.length; i++) {
+    if (name === users[i].name && pass === users[i].pass) {
+      req.name = users[i].name;
+      return next()
+    }
+  }
+  return res.status(404).json({
+    message: "Invalid credentials"
+  })
+}
+
+app.get('/health-checkup', authUser, (req, res) => {
+
 })
 
-app.get('/kidney-check', (req,res)=> {
+app.get('/kidney-check', authUser, (req, res) => {
 
 })
 
-app.get('/heart-check', (req,res)=> {
-  
+app.get('/heart-check', authUser, (req, res) => {
+
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Server is up at port ${port}`)
 })
