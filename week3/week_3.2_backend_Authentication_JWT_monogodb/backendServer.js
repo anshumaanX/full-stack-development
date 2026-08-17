5419288364901382678583d5aa2856667e6f22cf
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-17 07:36:22 +0000

    created in memory authentication system using JWT

---
+++ week3/week_3.2_backend_Authentication_JWT_monogodb/backendServer.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const z = require('zod');
+const jwt = require('jsonwebtoken');
+const port = 3000;
+
+const secretKey = 'wlpg';
+
+const app = express();
+app.use(express.json());
+
+const signinSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+const users = [
+  { id: 1, username: 'john_doe', password: 'password123' },
+  { id: 2, username: 'jane_smith', password: 'securepass' },
+  { id: 3, username: 'alice_jones', password: 'mypassword' },
+  { id: 4, username: 'bob_brown', password: 'passw0rd' },
+]
+
+app.post('/signin', (req, res) => {
+  if (!req.body) {
+    return res.status(411).json({
+      message: "Please enter username and password"
+    })
+  }
+  const result = signinSchema.safeParse(req.body);
+
+  if (!result.success) {
+    console.log("issues:", result.error.issues);
+
+    const issue = result.error.issues[0];
+
+    console.log("path:", issue.path);
+    console.log("message:", issue.message);
+
+    return res.status(400).json({
+      message: issue.message,
+      path: issue.path,
+    });
+  }
+
+  const { username, password } = result.data;
+  const user = users.find(u => u.username === username && u.password === password);
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid credentials"
+    })
+  }
+  const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '1h' });
+  res.status(200).json({
+    token
+  });
+});
+
+app.get('/users', (req, res) => {
+  const token = req.headers.authorization
+  if (!token) {
+    return res.status(401).json({
+      message: "No token provided"
+    })
+  }
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const { id, username } = decoded;
+
+    const user = users.find(u => u.id === id && u.username === username);
+    if(!user) {
+      return res.status(401).json({
+        message: "User not found!"
+      })
+    }
+    const filteredUsers = users.filter(u => u.id !== id && u.username !== username);
+    return res.status(200).json({
+      users: filteredUsers
+    })
+
+  } catch (err) {
+    return res.status(403).json({
+      message: "Invalid token",
+      error: err
+    })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
