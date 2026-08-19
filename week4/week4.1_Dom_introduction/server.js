3603dd2cb8d3f41966c12fd3759ea514b69fa908
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-19 15:21:50 +0000

    created and backend and fetched data using DOM

---
+++ week4/week4.1_Dom_introduction/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const PORT = 3000;
+
+app.use(cors())
+
+app.get('/sum', (req, res) => {
+  const { a, b } = req.query
+  console.log(a,b)
+  if (!a || !b) {
+    return res.status(400).json({
+      message: "Please provide both a and b!"
+    })
+  }
+
+  const sum = Number(a) + Number(b)
+
+  res.status(200).json({
+    message: `Sum of ${a} and ${b} is ${sum}`,
+    sum
+  })
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server is up at port ${PORT}`)
+})
