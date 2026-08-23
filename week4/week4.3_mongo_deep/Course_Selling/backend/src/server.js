b37628e46feaae14178d3001cf8c10f113bf3cb4
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-23 08:24:43 +0000

    added mongoose and connected to database

--- week4/week4.3_mongo_deep/Course_Selling/backend/src/server.js
+++ week4/week4.3_mongo_deep/Course_Selling/backend/src/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import app from "./app.js";
+import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,17 @@ app.get('/', (req,res) => {
   })
 })
 
-app.listen(PORT, ()=> {
-  console.log(`Server running on port ${PORT}`)
-})
+const startServer = async () => {
+  try{
+    await connectDB();
+
+    app.listen(PORT, ()=> {
+      console.log(`Server running on port ${PORT}`);
+    })
+  }catch(error){
+    console.error("Failed to start server:", error.message);
+    process.exit(1)
+  }
+}
+
+startServer()
