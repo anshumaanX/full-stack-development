34f3b26e2ef87d792586ed297be35d3a321df212
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-23 08:25:54 +0000

    created user schema

---
+++ week4/week4.3_mongo_deep/Course_Selling/backend/src/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
