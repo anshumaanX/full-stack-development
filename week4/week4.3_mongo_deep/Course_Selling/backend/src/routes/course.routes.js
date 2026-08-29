2e285901ee084153a9d5dfbfa482960886e52768
Anshumaan <anshumaan.tripathi.in@gmail.com>
2026-08-29 12:14:49 +0000

    added course routes

--- week4/week4.3_mongo_deep/Course_Selling/backend/src/routes/course.routes.js
+++ week4/week4.3_mongo_deep/Course_Selling/backend/src/routes/course.routes.js
@@ -1,12 +1,16 @@
 import express from 'express';
-import { createCourse, getCourseById, getCourses } from '../controllers/course.controller.js';
+import { createCourse, deleteCourse, getCourseById, getCourses, publishCourse, updateCourse } from '../controllers/course.controller.js';
 import authMiddleware from '../middleware/auth.middleware.js';
 import requireRole from '../middleware/role.middleware.js';
+import courseOwnershipMiddleware from "../middleware/course.middleware.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, requireRole("ADMIN"), createCourse)
 router.get("/", getCourses)
 router.get("/:id", getCourseById)
+router.put("/:id",authMiddleware,requireRole("ADMIN"),courseOwnershipMiddleware,updateCourse);
+router.delete("/:id",authMiddleware,requireRole("ADMIN"),courseOwnershipMiddleware,deleteCourse);
+router.patch("/:id/publish",authMiddleware,requireRole("ADMIN"),courseOwnershipMiddleware,publishCourse);
 
 export default router;
